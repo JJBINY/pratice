979799f27a5f9e08d33ea91ae5c45ca522000792
JJBINY <sisoya0424@gmail.com>
2025-03-19 09:22:29 +0900

    init: vt practice project

---
+++ 동시성/virtualtherad/src/main/java/com/example/virtualtherad/VirtualtheradApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualtherad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualtheradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualtheradApplication.class, args);
+	}
+
+}
---
+++ 동시성/virtualtherad/src/test/java/com/example/virtualtherad/VirtualtheradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualtherad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualtheradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
